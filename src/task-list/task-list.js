51b1a9719c07f4a01609b97fdfcd77765b9deb7a
tatyana-kuznecova <tatiana.kuzart@yahoo.com>
2025-06-05 20:08:49 +0400

    todo1 + edit + hook

--- src/task-list/task-list.js
+++ src/task-list/task-list.js
@@ -1,77 +1,72 @@
-import React, {Component} from 'react';
+import React, { useEffect, useState } from 'react';
 import Task from '../task';
 import PropTypes from 'prop-types';
 import './task-list.css';
 
-export default class TaskList extends Component {
-    componentDidMount() {
-        this.intervalId = setInterval(() => {
-            this.forceUpdate();
+const TaskList = ({
+                      todos = [],
+                      onDeleted = () => {},
+                      onToggleDone = () => {},
+                      onToggleEditMode = () => {},
+                      onEditTask = () => {},
+                      onToggleTimer = () => {},
+                  }) => {
+    const [, forceUpdate] = useState(0);
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            forceUpdate((n) => n + 1);
         }, 1000);
-    }
+        return () => clearInterval(intervalId);
+    }, []);
 
-    componentWillUnmount() {
-        clearInterval(this.intervalId);
-    }
+    const elements = todos.map(({ id, done, edit, label, timer, startTime, ...itemProps }) => {
+        let classNames = '';
+        if (done) classNames = 'completed';
+        if (edit) classNames = 'editing';
 
-    render() {
-        const {
-            todos,
-            onDeleted,
-            onToggleDone,
-            onToggleEditMode,
-            onEditTask,
-            onToggleTimer,
-        } = this.props;
-
-        const elements = todos.map(({ id, done, edit, label, timer, startTime, ...itemProps }) => {
-            let classNames = '';
-            if (done) classNames = 'completed';
-            if (edit) classNames = 'editing';
-
-            const editTask = (event) => {
-                if (event.key === 'Enter') {
-                    event.preventDefault();
-                    this.props.onEditTask(id, event.target.value);
-                }
-            };
-
-            let actualTimer = timer;
-            if (itemProps.isRunning && startTime) {
-                const diff = Math.floor((Date.now() - startTime) / 1000);
-                actualTimer += diff;
+        const editTask = (event) => {
+            if (event.key === 'Enter') {
+                event.preventDefault();
+                onEditTask(id, event.target.value);
             }
+        };
 
-            return (
-                <li key={id} className={classNames}>
-                    <Task
-                        label={label}
-                        done={done}
-                        edit={edit}
-                        timer={actualTimer}
-                        {...itemProps}
-                        onToggleTimer={() => onToggleTimer(id)}
-                        onDeleted={() => onDeleted(id)}
-                        onToggleDone={() => onToggleDone(id)}
-                        onToggleEditMode={() => onToggleEditMode(id)}
-                        onEditTask={onEditTask}
+        let actualTimer = timer;
+        if (itemProps.isRunning && startTime) {
+            const diff = Math.floor((Date.now() - startTime) / 1000);
+            actualTimer += diff;
+        }
+
+        return (
+            <li key={id} className={classNames}>
+                <Task
+                    label={label}
+                    done={done}
+                    edit={edit}
+                    timer={actualTimer}
+                    {...itemProps}
+                    onToggleTimer={() => onToggleTimer(id)}
+                    onDeleted={() => onDeleted(id)}
+                    onToggleDone={() => onToggleDone(id)}
+                    onToggleEditMode={() => onToggleEditMode(id)}
+                    onEditTask={onEditTask}
+                />
+                {edit && (
+                    <input
+                        className="edit"
+                        type="text"
+                        defaultValue={label}
+                        onKeyDown={editTask}
+                        autoFocus
                     />
-                    {edit ? (
-                        <input
-                            className="edit"
-                            type="text"
-                            defaultValue={label}
-                            onKeyDown={editTask}
-                            autoFocus
-                        />
-                    ) : null}
-                </li>
-            );
-        });
+                )}
+            </li>
+        );
+    });
 
-        return <ul className="todo-list">{elements}</ul>;
-    }
-}
+    return <ul className="todo-list">{elements}</ul>;
+};
 
 TaskList.propTypes = {
     todos: PropTypes.arrayOf(
@@ -93,11 +88,4 @@ TaskList.propTypes = {
     onToggleTimer: PropTypes.func,
 };
 
-TaskList.defaultProps = {
-    todos: [],
-    onDeleted: () => {},
-    onToggleDone: () => {},
-    onToggleEditMode: () => {},
-    onEditTask: () => {},
-    onToggleTimer: () => {},
-};
+export default TaskList;
--- src/task/task.js
+++ src/task/task.js
@@ -1,41 +1,38 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { formatDistance } from 'date-fns';
 import PropTypes from 'prop-types';
 import './task.css';
 
-export default class Task extends Component {
-    static propTypes = {
-        label: PropTypes.string.isRequired,
-        createdAt: PropTypes.instanceOf(Date).isRequired,
-        onDeleted: PropTypes.func,
-        onToggleDone: PropTypes.func,
-        onEditTask: PropTypes.func,
-        onToggleEditMode: PropTypes.func,
-        onToggleTimer: PropTypes.func,
-        done: PropTypes.bool,
-        isRunning: PropTypes.bool,
-        timer: PropTypes.number,
-        startTime: PropTypes.number,
-    };
+const Task = ({
+                  label,
+                  createdAt,
+                  onDeleted,
+                  onToggleDone,
+                  onEditTask,
+                  onToggleEditMode,
+                  onToggleTimer,
+                  done = false,
+                  isRunning = false,
+                  timer = 0,
+                  startTime = 0,
+              }) => {
+    const [, forceUpdate] = useState(0); // Используем хак для обновления каждую секунду
 
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            forceUpdate((n) => n + 1);
+        }, 1000);
+        return () => clearInterval(intervalId);
+    }, []);
 
-    componentDidMount() {
-        this.intervalId = setInterval(() => this.forceUpdate(), 1000);
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.intervalId);
-    }
-
-    formatTime(totalSeconds) {
+    const formatTime = (totalSeconds) => {
         const hours = String(Math.floor(totalSeconds / 3600)).padStart(2, '0');
         const minutes = String(Math.floor((totalSeconds % 3600) / 60)).padStart(2, '0');
         const seconds = String(totalSeconds % 60).padStart(2, '0');
         return `${hours}:${minutes}:${seconds}`;
-    }
+    };
 
-    getDisplayTimer() {
-        const { timer = 0, isRunning, startTime } = this.props;
+    const getDisplayTimer = () => {
         let total = timer;
 
         if (isRunning && startTime) {
@@ -43,52 +40,55 @@ export default class Task extends Component {
             total += diff;
         }
 
-        return this.formatTime(total);
-    }
+        return formatTime(total);
+    };
 
-    render() {
-        const {
-            label,
-            createdAt,
-            onDeleted,
-            onToggleDone,
-            done,
-            onToggleEditMode,
-            isRunning,
-            onToggleTimer,
-        } = this.props;
-
-        return (
-            <div className="view">
-                <input
-                    className="toggle"
-                    type="checkbox"
-                    checked={done}
-                    readOnly={true}
-                    onClick={onToggleDone}
-                />
-                <div id="label">
-                    <span className="title" onClick={onToggleDone}>
-                        {label}
-                    </span>
-                    <span className="description">
-                        {isRunning ? (
-                            <button className="icon icon-pause" onClick={onToggleTimer}></button>
-                        ) : (
-                            <button className="icon icon-play" onClick={onToggleTimer}></button>
-                        )}
-                        {this.getDisplayTimer()}
-                    </span>
-                    <span className="description">
-                        {formatDistance(createdAt, new Date(), {
-                            addSuffix: true,
-                            includeSeconds: true,
-                        })}
-                    </span>
-                </div>
-                <button className="icon icon-edit" onClick={onToggleEditMode}></button>
-                <button className="icon icon-destroy" onClick={onDeleted}></button>
+    return (
+        <div className="view">
+            <input
+                className="toggle"
+                type="checkbox"
+                checked={done}
+                readOnly={true}
+                onClick={onToggleDone}
+            />
+            <div id="label">
+        <span className="title" onClick={onToggleDone}>
+          {label}
+        </span>
+                <span className="description">
+          {isRunning ? (
+              <button className="icon icon-pause" onClick={onToggleTimer}></button>
+          ) : (
+              <button className="icon icon-play" onClick={onToggleTimer}></button>
+          )}
+                    {getDisplayTimer()}
+        </span>
+                <span className="description">
+          {formatDistance(createdAt, new Date(), {
+              addSuffix: true,
+              includeSeconds: true,
+          })}
+        </span>
             </div>
-        );
-    }
-}
+            <button className="icon icon-edit" onClick={onToggleEditMode}></button>
+            <button className="icon icon-destroy" onClick={onDeleted}></button>
+        </div>
+    );
+};
+
+Task.propTypes = {
+    label: PropTypes.string.isRequired,
+    createdAt: PropTypes.instanceOf(Date).isRequired,
+    onDeleted: PropTypes.func,
+    onToggleDone: PropTypes.func,
+    onEditTask: PropTypes.func,
+    onToggleEditMode: PropTypes.func,
+    onToggleTimer: PropTypes.func,
+    done: PropTypes.bool,
+    isRunning: PropTypes.bool,
+    timer: PropTypes.number,
+    startTime: PropTypes.number,
+};
+
+export default Task;
